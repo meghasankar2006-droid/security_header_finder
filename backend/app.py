f7269b496216e04bbe96cab8239ee424378f7c79
Meghasri <meghasankar2006@gmail.com>
2026-04-26 23:18:36 +0530

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -3,6 +3,8 @@ SHIELD ANALYZER - Security Header Intelligence Engine & Logging Dashboard
 Backend: Flask API Server
 Author: ShieldAnalyzer Team
 """
+
+
 from flask import Flask, request, jsonify, send_from_directory
 from flask_cors import CORS
 import requests
